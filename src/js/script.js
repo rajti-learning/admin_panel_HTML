0e4e0b1eb1393e728ea97e647bcb51111381b75b
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-05-07 21:43:19 +0200

    Finalize datePicker

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,21 @@ document.querySelector('.hamburger_button').addEventListener('click', function(e
     event.preventDefault();
     toggleMenu();
 });
+
+function datePicker() {
+    const dataField = document.querySelector('.lol');
+    
+    flatpickr(dataField, {
+        dateFormat: "d-m-Y",
+        mode: "range",
+    });
+}
+
+function buttonBlocker() {
+    document.querySelector('.refresh').addEventListener('click', function(event) {
+        event.preventDefault();
+    });
+}
+
+datePicker();
+buttonBlocker();
